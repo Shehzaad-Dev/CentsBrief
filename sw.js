19e954b57536eb3365202dde23df30b17ed818c7
Shehzaad <shehzadwazir911@gmail.com>
2026-05-21 03:36:42 +0500

    Remove popender ads - keep only 6 banner ads with 1-minute auto-refresh

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11028447
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+// Service worker - no external ad scripts
